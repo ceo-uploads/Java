8f1c1cbcc39d7273365eddbc57989920a1bd54a1
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-07-11 15:04:03 +0600

    Add files via upload

---
+++ javaOutput/Numbers.java
@@ -0,0 +1,10 @@
+package javaOutput;
+
+public class Numbers {
+    public static void main(String[] args) {
+        System.out.println("3");
+        System.out.println("30");
+        System.out.println("300");
+        System.out.println("3000");
+    }
+}
---
+++ javaOutput/Text.java
@@ -0,0 +1,9 @@
+package javaOutput;
+
+public class Text {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("I'm Learning Java");
+        System.out.println("It's pretty");
+    }
+}
