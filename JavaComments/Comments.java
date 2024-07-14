4992995d4bbfe7cc60c21f07bda6e91cccbcafa1
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-07-15 00:06:19 +0600

    Add files via upload

---
+++ JavaComments/Comments.java
@@ -0,0 +1,16 @@
+package JavaComments;
+
+public class Comments {
+    public static void main(String[] args) {
+        // this is an example of single line comment....
+
+        System.out.println("Hello World!");
+
+        /*
+         * This is an example of multiple line comments..
+         * Always remember that, comments are directly ignored
+         * by the compiler in the time of execution and has no impact
+         * on the code.. it is used for various flexiblity..
+         */
+    }
+}
