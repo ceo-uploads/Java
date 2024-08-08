62cc7fbe9daade90f77f8b7e546067722ea8717b
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-08 23:26:27 +0600

    WideningCasting.java
    (automatically) - converting a smaller type to a larger type size.. 
    Type casting is when you assign a value of one primitive data type to another type.

---
+++ WideningCasting.java
@@ -0,0 +1,9 @@
+class WideningCasting;
+
+public static void main(string[] args) {
+  int number = 9; // instances..
+  double myNumber = number; // casting - int ->> double..
+
+  System.out.println("Number befoer Widening Typecasing: "+number);  // it will print 9...
+  System.out.println("Number after Widening Typecasing: "+myNumber)  // ot woll print 9.0 ... 
+}
