cbf54289fe8e5415ab4a474c3cf162557fc20900
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-09 21:54:54 +0600

    java Basic Operators
    Commonly used operators in Java.

---
+++ javaOperatorEx1.java
@@ -0,0 +1,22 @@
+class javaOperatorEx1 {
+  public static void main(String[] arg) {
+    // instances..
+    int a = 10;
+    int b = 5;
+
+    // Summation of two variables..
+    System.out println("Sum = "+(a+b)); // you can try - int sum = a + b...
+
+    // Substraction of two variables..
+    System.out println("Sub = "+(a-b)); // you can try - int sub = a - b...
+
+    // Multiplication of two vatiables..
+    System.out println("Mul = "+(a*b)); // you can try - int mul = a * b...
+
+    // Division of two variables..
+    System.out println("Div = "+(a/b)); // you can try - int div = a / b...
+
+    // Modulas of two variables..
+    System.out println("Mod = "+(a%b)); // you can try - int mod = a % d...
+  }
+}
