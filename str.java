ef8506dc1fdc8a2eca9d57fb3bd4497cd1c3938c
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-09 22:04:01 +0600

    String in java
    Basic String in Java..

---
+++ str.java
@@ -0,0 +1,7 @@
+class str {
+  public static void main(String[] args) {
+    String txt = "Hello"; // invoking a String variable in java...
+
+    System.out println(txt); // printing the String variable..
+  } 
+}
