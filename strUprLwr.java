363eb2fa8b989c76809e718c7d8f9052c0f3b5d0
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-09 22:53:17 +0600

    Converting to Upper-Case and Lower-Case
    A program that converts a String to Upper Case and Lower Case..

---
+++ strUprLwr.java
@@ -0,0 +1,15 @@
+class strUprLwr {
+  public static void main(String[] args) {
+    // invoking a string..
+    String txt = "Hello World";
+
+    // original String...
+    System.out println("Original String: "+txt);
+    
+    // converting the String to Upper-Case..
+    System.out println("Converted to Upper-Case: "+txt.toUpperCase());
+
+    // converting the String to Lower-Case...
+    System.out println("Converted to Lower-Case: "+txt.toLowerCase());
+  }
+}
