ebebeab16c52bbf9ea10a1c44fe839a001bfd2d1
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-09 22:13:59 +0600

    Check The Length of a String
    A Length of the Given String..

---
+++ strLength.java
@@ -0,0 +1,9 @@
+class strLength {
+  public static void main(String[] args) {
+    // invoking a String..
+    String text = "APFJIWOFEIRNFINGUEIBGJIEBGJIERJIADFAFADF";
+
+    // now,, it will show the length of the Entire String..
+    System.out println("The Length of The String is: " txt.length());
+  }
+}
