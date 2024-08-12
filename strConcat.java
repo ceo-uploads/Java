e4926539cde8fe858a547630b73b8709490e629a
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-12 12:05:41 +0600

    Concatenation of two  Strings
    this program Shows how to concatenation going through java..

---
+++ strConcat.java
@@ -0,0 +1,10 @@
+class strConcat {
+  public static void main(String[] args) {
+    // String instances...
+    String ch1 = "Hello";
+    String ch2 = "World!";
+
+    // Concating the Strings...
+    System.out.println(ch1.concat(ch2));
+  }
+}
