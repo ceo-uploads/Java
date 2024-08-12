ba3a5615eccbda381c89a2e37c60f600302b0df6
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-12 11:57:06 +0600

    Arithmetic Operator
    It is an example of arithmetic operator...

---
+++ arithmeticOperator.java
@@ -0,0 +1,13 @@
+class arithmeticOperator {
+  public static void main(String[] args) {
+    // instances...
+    int a = 100;
+    int b = 200;
+
+    int sum = a + b;  // sum,,
+    int sum1 = sum + a + b;  // incremental operator or arithmetic operator...
+    int sum2 = sum + sum1;
+
+    System.out.println(sum+"\n"+sum1+"\n"+sum2);
+  }
+}
