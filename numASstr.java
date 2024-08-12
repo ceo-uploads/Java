2ddc5bdf5b2cc470c13651ea52fbb909ce56a564
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-12 12:14:28 +0600

    Numbers as String operation in java
    How Numbers reacts as a string.

---
+++ numASstr.java
@@ -0,0 +1,10 @@
+class numASstr {
+  public static void main(String[] args) {
+    // instances...
+    String x = "10";
+    String y = "20";
+    String z = x + y; // this will show 1020 as output...
+
+    System.out.println(z);
+  }
+}
