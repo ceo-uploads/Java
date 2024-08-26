13e596bfeba262e25f75d0c8eb2ad2f1e2c40740
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-26 22:46:15 +0600

    To generate a random number using math operation.

---
+++ randomNumberGenerator.java
@@ -0,0 +1,7 @@
+class randomNumberGenerator {
+  public static void main(String[] arsg) {
+    int random = (int) (Math.random() * 101); // 0 to 100
+    // displaying the random number...
+    System.out.print("The random number is: " +random);
+  }
+}
