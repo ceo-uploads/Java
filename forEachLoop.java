c2664776fdff6df077996ad3eb38bed6a71a4d85
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-28 22:44:45 +0600

    For Each loop in java.

---
+++ forEachLoop.java
@@ -0,0 +1,10 @@
+class forEachLoop {
+  public static void main(String[] args) {
+    // instances- character array...
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    // for each loop- till traversing the every element in the array...
+    for (int i : cars) {
+      System.out.println(i);
+    }
+  }
+}
