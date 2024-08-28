05dfdd13268025811cff1bc1da02807e91cc24ea
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-28 22:34:15 +0600

    For loop in java.

---
+++ forLoop.java
@@ -0,0 +1,12 @@
+class forLoop {
+  public static void main(String[] args) {
+    // instances...
+    int i;
+
+    // for loop- till i < 5...
+    for(i = 0; i < 5; i++) {
+      System.out.println(i);
+      i++;
+    }
+  }
+}
