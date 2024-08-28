7e2555f0c68ebf07d1c54500105d8a29c1a8400e
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-28 22:01:52 +0600

    Short hand conditioning in java.

---
+++ shortHandifElse.java
@@ -0,0 +1,11 @@
+class shortHandifElse {
+  public static void main(String[] args) {
+    // instances...
+    int temp = 20;
+    // example of Shorthand conditioning...
+    String result = (temp < 18) ? "It's Normal": "It's Hot";
+
+    // expecting output...
+    System.out.println(result);
+  }
+}
