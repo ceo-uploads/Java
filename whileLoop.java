4d3e8479d3f6009ae79372fb39357f3007c493b0
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-28 22:12:46 +0600

    While Loop in java.

---
+++ whileLoop.java
@@ -0,0 +1,11 @@
+class whileLoop {
+  public static void main(String[] args) {
+    // instances...
+    int i = 0;
+    // while loop since i < 5...
+    while (i < 5) {
+      System.out.println(i);
+      i++;
+    }
+  }
+}
