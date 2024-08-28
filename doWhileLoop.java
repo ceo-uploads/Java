6a3fdd8f4313fdfe31c827274b4da5423d7c86b2
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-28 22:16:23 +0600

    Do While Loop in java.

---
+++ doWhileLoop.java
@@ -0,0 +1,11 @@
+class doWhileLoop {
+  public static void main(String[] args) {
+    // instances...
+    int i = 0;
+    // Do While Loop...
+    do {
+      System.out.println(i);
+      i++;
+    } while (i < 5); // the loop will continue since i < 5...
+  }
+}
