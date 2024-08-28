6e75759c46e9edcf547b21ceab4da35eafc27225
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-08-28 21:54:21 +0600

    if/else/else if condition in java

---
+++ ifElseCondition.java
@@ -0,0 +1,15 @@
+class ifElseCondition {
+  public static void main(String[] args) {
+    // instances..
+    int time = 22;
+    // branh-- if/else condition...
+    if (time < 10) {
+       System.out.println("Good morning.");
+    } else if (time < 18) {
+       System.out.println("Good day.");
+    } else {
+       System.out.println("Good evening.");
+    }
+    // Outputs "Good evening."
+  }
+}
